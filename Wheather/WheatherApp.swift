34bf2278ecdeeedd764598d438bb3902247041d0
fusethnd <fusethnd@gmail.com>
2024-02-23 10:10:59 +0700

    Initial Commit

---
+++ Wheather/WheatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WheatherApp.swift
+//  Wheather
+//
+//  Created by Thanadon Boontawee on 23/2/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct WheatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
